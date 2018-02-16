d8e21014c1c6f673e13f4e69a6434011571fd32f
Nan-L <nanliuca@outlook.com>
2018-02-15 18:00:35 -0800

    added login

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  resources :users
+
+  get    'login/index'
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  root 'login#index'
+
+end
