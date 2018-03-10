0064815b2536b287d71beffda2af3f183e7fdb14
Nan-L <nanliuca@outlook.com>
2018-03-09 17:56:43 -0800

    added book

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   resources :users
+  resources :books
 
   get    'login/index'
   get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
+  get    '/sell',    to:  'books#new'
   post   '/login',   to: 'sessions#create'
+  post   '/sell',    to: 'books#create'
   delete '/logout',  to: 'sessions#destroy'
+
 
   root 'login#index'
 
