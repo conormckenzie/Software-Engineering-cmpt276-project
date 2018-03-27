52014e21a3a435bc617b6647476b5be6bfbee674
Nan-L <nanliuca@outlook.com>
2018-03-26 17:24:45 -0700

    added several validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,10 @@
 class Book < ApplicationRecord
   validates :title,  presence: true, length: { maximum: 50 }
-  validates :year, presence: true, length: { maximum: 10 }
-  validates :isbn, presence: true, length: { maximum: 20 }
+  validates :year, presence: true, length: { is: 4 }, numericality: { only_integer: true}
+  validates :isbn, presence: true, length: { is: 13 }, numericality: { only_integer: true}
+  validates :author, presence: true, length: { maximum: 20 }
+  validates :price, presence: true, length: { maximum: 10 }, numericality: true
+  validates :course_number, presence: false, length: { maximum: 10 }
+  validates :image, presence: false, length: {maximum: 10 }
+
 end
