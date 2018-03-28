ad12c4c1c95cc19819b02dfe7f0a11865aad889c
Md Shahriar Rahman <msrahman@sfu.ca>
2018-03-27 19:59:25 -0700

    remember me

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180326233458) do
+ActiveRecord::Schema.define(version: 20180327232921) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20180326233458) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
