4252a53cb5860cde44ddf498116c6b5f983c5251
jdsgfser <wangzongqi@msn.com>
2018-03-28 03:12:32 +0000

    resolved conflict

--- db/schema.rb
+++ db/schema.rb
@@ -25,13 +25,9 @@ ActiveRecord::Schema.define(version: 20180327232921) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email"
     t.string "password"
-    t.string "provider"
-    t.string "uid"
-    t.string "name"
-    t.string "oauth_token"
-    t.datetime "oauth_expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
