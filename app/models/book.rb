b3feafb8cca66ea2b02428939584d79ad7e0ab59
conormckenzie <conor.mckenzie314@hotmail.com>
2018-03-28 18:50:17 -0700

    Increase maximums on attribute length
    Title max length now 200 instead of 50
    Author max length now 100 instead of 20

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,9 @@
 class Book < ApplicationRecord
   mount_uploader :image, AvatarUploader
-  validates :title,  presence: true, length: { maximum: 50 }
+  validates :title,  presence: true, length: { maximum: 200 }
   validates :year, presence: true, length: { is: 4 }, numericality: { only_integer: true}
   validates :isbn, presence: true, length: { is: 13 }, numericality: { only_integer: true}
-  validates :author, presence: true, length: { maximum: 20 }
+  validates :author, presence: true, length: { maximum: 100 }
   validates :price, presence: true, length: { maximum: 10 }, numericality: true
   validates :course_number, presence: false, length: { maximum: 10 }
   validates :image, presence: false
